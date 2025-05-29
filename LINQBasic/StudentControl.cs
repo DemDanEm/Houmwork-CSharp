5d1471853a7de8b662e4a1ec081e2c4c17174385
DemDanEm <scholschit@gmail.com>
2025-05-29 06:46:15 +0300

    the unlucky numbered homework

---
+++ LINQBasic/StudentControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQBasic
+{
+    internal class StudentControl
+    {
+        public static double student_avg(Student student)
+        {
+            return student.Grades.Average();
+        }
+
+        public static List<Student> student_cut_avg(List<Student> list, double targetAvg)
+        {
+            return list.Where(stud => student_avg(stud) >= targetAvg).ToList();
+        }
+
+        public static List<Student> student_sorted(List<Student> students)
+        {
+            return students.OrderBy(stud=> stud.LastName).ToList();
+        }
+
+        public static List<Student> student_has_grade(List<Student> students, int grade)
+        {
+            return students.Where(stud=> stud.Grades.Any(gr => grade == gr )).ToList();
+        }
+
+        public static Dictionary<string, double> sudent_avg_by_group(List<Student> students)
+        {
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+            
+            foreach(var group in students.GroupBy(stud=> stud.Group))
+            {
+                int avg = 0;
+                foreach (var student in group)
+                {
+                    avg += student.Grades.Sum();
+                }
+                avg = avg / students.Count();
+                dict.Add(group.Key, avg);
+            }
+            return dict;
+        }
+
+
+        public static List<string> that_one(List<Student> students, string targetGroup, int targetGrade)
+        {
+            return students
+                .Where(stud => stud.Group == targetGroup)
+                .Where(stud => student_avg(stud) >= targetGrade)
+                .OrderBy(stud => stud.FirstName)
+                .Select(stud => stud.LastName).ToList();s
+
+        }
+    }
+}
---
+++ LINQBasic/Sudent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQBasic
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Group { get; set; }
+        public int[] Grades { get; set; }
+        public Student(string firstName, string lastName, string group, int[] grades)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Group = group;
+            Grades = grades;
+        }
+
+        public static List<Student> fish()
+        {
+            List<Student> list = new List<Student>();
+            list.Add(new Student("Dave", "The", "Gobbers", new int[] { 2, 4, 5, 5, 3 }));
+            list.Add(new Student("Wave", "Move", "Wavers", new int[] { 3, 3, 3, 3 }));
+            list.Add(new Student("Cave", "Diver", "Gobbers", new int[] { 2, 4, 5, 5, 3 }));
+            list.Add(new Student("Save", "Tail", "Critters", new int[] { 1, 1, 1 , 1 }));
+            list.Add(new Student("Knave", "Knight", "Gobbers", new int[] { 2, 4, 5, 5, 3 }));
+            list.Add(new Student("Pave", "Walk", "Critters", new int[] { 2, 3, 4, 4, 3, 2 }));
+
+            return list;
+        }
+
+    }
+
+}
