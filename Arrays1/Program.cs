6a6c0278bea0a09e9d8f5bd7698a9a2032e35ad7
DemDanEm <scholschit@gmail.com>
2025-05-29 05:03:00 +0300

    Arrays\ Homework2

---
+++ Arrays1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Arrays1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Фантастика", "Криминал", "Экшн" };
+
+            string[][] movies = {
+                new string[] { "Матрица", "Интерстеллар", "Время" }, // Фантастика
+                new string[] { "Крёстный отец", "Казино", "Славные парни" }, // Криминал
+                new string[] { "Аватар", "Человек-паук", "Железный человек" } // Экшн
+                };
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine(names[i] + ":");
+                foreach (string film in movies[i])
+                {
+                    Console.WriteLine("- " + film);
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
