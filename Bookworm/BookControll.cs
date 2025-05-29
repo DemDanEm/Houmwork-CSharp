116e5c1304dc93b758c64a98f36fdc032c30d593
DemDanEm <scholschit@gmail.com>
2025-05-29 05:44:48 +0300

    When the homework is 3

---
+++ Bookworm/BookControll.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookworm
+{
+    internal class BookControl
+    {
+        Stack<string> books = new Stack<string>();
+
+        public void add(string book)
+        {
+            books.Push(book);
+        }
+
+        public string remove(string book)
+        {
+            return books.Pop();
+        }
+
+        public string top()
+        {
+            return books.First();
+        }
+
+        public List<string> all()
+        {
+            List<string> list = new List<string>();
+            foreach (string book in books)
+            {
+                list.Add(book);
+            }
+            return list;
+        }
+
+    }
+}
---
+++ GamingInventory/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingInventory
+{
+    internal class Inventory
+    {
+        Dictionary<string, int> inventory = new Dictionary<string, int>();
+        public Inventory() { }
+
+        public void add_item(string item, int amount = 1)
+        {
+            if (inventory.ContainsKey(item))
+            { inventory[item] += amount; }
+            else { inventory[item] = amount; }
+        }
+
+        public void remove_item(string item, int amount)
+        {
+            if (inventory.ContainsKey(item))
+            {
+                if (amount > inventory[item])
+                {
+                    inventory.Remove(item);
+                }
+                else
+                {
+                    inventory[item] -= amount;
+                }
+            }
+        }
+        public void print()
+        {
+            foreach(var key in inventory.Keys) 
+                {
+                Console.WriteLine(key + ":" + inventory[key].ToString());
+            }
+        }
+
+        public int search(string name)
+        {
+            if (inventory.ContainsKey(name))
+            {
+                return inventory[name];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
